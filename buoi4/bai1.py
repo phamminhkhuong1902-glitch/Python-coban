758c0d5202b16d1e297c7e71dd3f60be22af0e8a
Pham Minh Khuong <phamminhkhuong1902@gmail.com>
2025-11-24 16:41:35 +0700

    them buoi1 bo sung

--- buoi4/bai1.py
+++ buoi4/bai1.py
@@ -37,8 +37,54 @@ while(i<=10):
         print ("Lỗi kiểu dữ liệu")
 
 print (f"Có {dem} số âm trong 10 số")
-print ("---CÂU 4---")
+
+print ("---CÂU 5---")
+
 n=int(input("Nhập n:"))
 print (f"Bảng cửu chương {n}")
 for i in range(1,11):
     print(f"{n} x {i} ={n*i}")
+
+print ("---CÂU 6---")
+for i in range(1,N+1):
+    if i==5:
+        continue
+    print(i,end=" ")
+
+print ("---CÂU 6---")
+max=-1000
+min=1000
+i=1
+while(i<=n):
+    try:
+        x=int(input(f"Nhập số thứ {i}:"))
+        i+=1
+        if x > max:
+            max=x
+        if x < min:
+            min=x
+    except:
+        print ("Lỗi kiểu dữ liệu")
+print(f"Số lớn nhất:{max}")
+print(f"Số nhỏ nhất:{min}")
+print ("---CÂU 8---")
+while(i<=n):
+    try:
+        x=int(input(f"Nhập số thứ {i}:"))
+        i+=1
+        if x > max:
+            max=x
+        if x < min:
+            min=x
+    except:
+        print ("Lỗi kiểu dữ liệu")
+print(f"Số lớn nhất:{max}")
+print(f"Số nhỏ nhất:{min}")
+print ("---CÂU 9---")
+so=int(input(f"Nhập số:"))
+dem=0
+so1=so
+while(so!=0):
+    so=so//10
+    dem+=1
+print(f"Số {so1} có {dem} chữ số")
